9ecc72a3dcf42f2a0bc0cdbbe36a2029598a606c
Vladimir Oltean <olteanv@gmail.com>
2025-03-08 23:28:30 +0200

    board/lx2160acex7: use custom DPL and DPC files
    ls-addsw --num-ifs=5 --max-fdbs=5 --flooding-cfg=DPSW_FLOODING_PER_FDB \
    	--broadcast-cfg=DPSW_BROADCAST_PER_FDB dpmac.7 dpmac.8 dpmac.9 dpmac.10
    restool dprc generate-dpl > clearfog-custom-dpl.dtb
    
    Signed-off-by: Vladimir Oltean <olteanv@gmail.com>

--- board/solidrun/lx2160acex7/post-build.sh
+++ board/solidrun/lx2160acex7/post-build.sh
@@ -5,8 +5,8 @@ PARTUUID="$("$HOST_DIR/bin/uuidgen")"
 
 install -d "$TARGET_DIR/boot/extlinux/"
 # Default DPL and DPC selections for mcinitcmd
-ln -sf clearfog-cx-s1_8-s2_0-dpc.dtb "$TARGET_DIR/boot/dpc.dtb"
-ln -sf clearfog-cx-s1_8-s2_0-dpl.dtb "$TARGET_DIR/boot/dpl.dtb"
+ln -sf clearfog-custom-dpc.dtb "$TARGET_DIR/boot/dpc.dtb"
+ln -sf clearfog-custom-dpl.dtb "$TARGET_DIR/boot/dpl.dtb"
 sed "s/%PARTUUID%/$PARTUUID/g" "$BOARD_DIR/extlinux.conf" > "$TARGET_DIR/boot/extlinux/extlinux.conf"
 sed "s/%PARTUUID%/$PARTUUID/g" "$BOARD_DIR/genimage.cfg" > "$BINARIES_DIR/genimage.cfg"
 install -d -m 0700 "$TARGET_DIR/root/.ssh/"
